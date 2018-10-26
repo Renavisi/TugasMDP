327699a14fdd2de6ff404b28471cab57c4b84dbe
Renavisi <Renavisi@users.noreply.github.com>
2018-10-26 21:04:14 +0700

    Modul2Kelompok33
    Modul2Kelompok33

---
+++ Android Studio Projects/app/src/main/java/com/example/rena/aplikasimodul2kel33/adapter/car/CarListerner.java
@@ -0,0 +1,7 @@
+package com.example.rena.aplikasimodul2kel33.adapter.car;
+
+import com.example.rena.aplikasimodul2kel33.data.model.DataCar;
+
+public interface CarListerner {
+    void onCarClick(DataCar dataCar);
+}
---
+++ Android Studio Projects/app/src/main/java/com/example/rena/aplikasimodul2kel33/ui/home/HomeView.java
@@ -0,0 +1,11 @@
+package com.example.rena.aplikasimodul2kel33.ui.home;
+
+import com.example.rena.aplikasimodul2kel33.data.model.DataCar;
+
+import java.util.List;
+
+public interface HomeView {
+    void successShowCar(List<DataCar> dataCars);
+    void failedShowCar(String message);
+
+}

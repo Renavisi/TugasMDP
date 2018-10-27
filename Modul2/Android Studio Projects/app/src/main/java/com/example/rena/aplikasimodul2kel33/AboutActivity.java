ea13d82d4602c6f6b7adbeca4a218b07fa539d26
Renavisi <Renavisi@users.noreply.github.com>
2018-10-27 13:31:46 +0700

    Modul2Kelompok33
    modul2kelompok33

---
+++ Modul2/Android Studio Projects/app/src/main/java/com/example/rena/aplikasimodul2kel33/AboutActivity.java
@@ -0,0 +1,35 @@
+package com.example.rena.aplikasimodul2kel33;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public void onBackPressed() {
+        back();
+    }
+
+    private void back() {
+        Intent reg = new Intent(this, MainActivity.class);
+        startActivity(reg);
+        finish();
+    }
+}
